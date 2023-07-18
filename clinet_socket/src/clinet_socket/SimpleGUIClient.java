0a674ae4aba7a06bf5bd350ebf538ede79581c75
dpung1 <pung_12@naver.com>
2023-07-18 17:40:52 +0900

    코드 보고 실행과정 주석처리 -ing

--- clinet_socket/src/clinet_socket/SimpleGUIClient.java
+++ clinet_socket/src/clinet_socket/SimpleGUIClient.java
@@ -94,13 +94,17 @@ public class SimpleGUIClient extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					// 프레임에 클라이언트의 인스턴스 생성
 					SimpleGUIClient frame = SimpleGUIClient.getInstance();
+					// 프레임을 화면에 표시
 					frame.setVisible(true);
-					
+					// 클라이언트리시버 객체 생성 (쓰레드)
 					ClientReceiver clientReceiver = new ClientReceiver();
+					// 쓰레드 실생
 					clientReceiver.start();
-					
+					// 커넥션 타입의 생성 후 데이터를 requestBodyDto 할당 (소켓의 커넥션)
 					RequestBodyDto<String> requestBodyDto = new RequestBodyDto<String>("connection", frame.username);
+					// 데이터를 클라이언트로 전송
 					ClientSender.getInstance().send(requestBodyDto);
 					
 				} catch (Exception e) {
@@ -113,13 +117,14 @@ public class SimpleGUIClient extends JFrame {
 	public SimpleGUIClient() {
 		
 		try {
+			// 127.0.0.1 IP 주소와 8000포트 소켓 생성
 			socket = new Socket("127.0.0.1", 8000);
 			
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
 		
-		//메인패널
+		// 메인패널
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 429, 597);
 		mainCardLayout = new CardLayout();
@@ -127,7 +132,7 @@ public class SimpleGUIClient extends JFrame {
 		mainCardPanel.setLayout(mainCardLayout);
 		setContentPane(mainCardPanel);
 		
-        //로그인패널
+        // 로그인패널
         loginPanel = new JPanel();
         loginPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
         loginPanel.setLayout(null);
@@ -138,28 +143,31 @@ public class SimpleGUIClient extends JFrame {
         
         mainCardPanel.add(loginPanel, "loginPanel");
         
-        //로그인 아이디 필드
+        // 로그인 아이디 필드
         usernameTextField = new JTextField();
         usernameTextField.setFont(new Font("HY엽서M", Font.PLAIN, 15));
         usernameTextField.setEnabled(true);
         usernameTextField.addKeyListener(new KeyAdapter() {
         	@Override
+        	// 엔터키 눌렀을경우 실행
         	public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-
+                	// 입력창에 입력된 값을 username에 할당
                     username = usernameTextField.getText();
-
+                    // 만약 공백인 경우 에러표시 후 종료
                     if (username.isBlank()) {
                         JOptionPane.showMessageDialog(loginPanel, "닉네임을 확인해주세요.", "입장 실패", JOptionPane.ERROR_MESSAGE);
                         return;
                     }
-
+                    
                     JOptionPane.showMessageDialog(loginPanel, "환영합니다. " + username + " 님!", "로그인 성공", JOptionPane.PLAIN_MESSAGE);
-
+					// 커넥션 타입의 생성 후 데이터를 requestBodyDto 할당 (소켓의 커넥션)
                     RequestBodyDto<String> requestBodyDto = new RequestBodyDto<>("connection", username);
+					// 데이터를 클라이언트로 전송
                     ClientSender.getInstance().send(requestBodyDto);
-
+                    // 라벨 "username님" 표시
                     usernameLabel.setText(username + "  님");
+                    // 대기실로 이동
                     mainCardLayout.show(mainCardPanel, "chattingRoomListPanel");
                 }
             }
@@ -170,12 +178,13 @@ public class SimpleGUIClient extends JFrame {
         usernameTextField.setEnabled(true);
         loginbackground.add(usernameTextField);
 
-        //로그인 버튼
+        // 로그인 버튼
         loginButton = new JButton("로그인");
         usernameTextField.setFont(new Font("HY엽서M", Font.PLAIN, 13));
         loginButton.setBounds(282, 470, 108, 41);
         loginButton.addMouseListener(new MouseAdapter() {
             @Override
+            // 위에 엔터키로 실행이 되었으면 해당 코드는 로그인버튼 클릭시 실행
             public void mouseClicked(MouseEvent e) {
                 username = usernameTextField.getText();
 
@@ -197,7 +206,7 @@ public class SimpleGUIClient extends JFrame {
         });
         loginbackground.add(loginButton);
 		
-        //대기실
+        // 대기실
 		chattingRoomListPanel = new JPanel();
 		chattingRoomListPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		chattingRoomListPanel.setLayout(null);
@@ -216,7 +225,7 @@ public class SimpleGUIClient extends JFrame {
 		chattingRoomListPanel.add(usernameLabel);
 
 		
-		//대기실 방 목록
+		// 대기실 방 목록
 		roomListScrollPanel = new JScrollPane(); 
 		roomListScrollPanel.setBounds(69, 155, 334, 395);
 		chattingRoomListPanel.add(roomListScrollPanel);
@@ -231,12 +240,17 @@ public class SimpleGUIClient extends JFrame {
 		roomList.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				if(e.getClickCount() == 2) {	//두번클릭 확인
+				// 방 리스트 더블 클릭시 실행
+				if(e.getClickCount() == 2) {
+					// 선택한 방 인덱스를 roomName에 할당 
 					roomName = roomListModel.get(roomList.getSelectedIndex());	//선택된 방 인덱스번호를 가져와 모델에서 get 해서 roomName 가져옴
-					
+					// 라벨에 "방제목 : roomName" 표시
 					roomLabel.setText("방제목:   " + roomName);
+					// 채팅방으로 이동
 					mainCardLayout.show(mainCardPanel, "chattingRoomPanel");
+					// 조인 타입의 생성 후 데이터를 requestBodyDto 할당 (소켓의 커넥션)
 					RequestBodyDto<String> requestBodyDto = new RequestBodyDto<String>("join", roomName);
+					// 데이터를 클라이언트로 전송
 					ClientSender.getInstance().send(requestBodyDto);
 					
 				}
@@ -245,36 +259,47 @@ public class SimpleGUIClient extends JFrame {
 		
 		roomListScrollPanel.setViewportView(roomList);
 		
-		//방만들기 버튼
+		// 방만들기 버튼
 		createRoomButton = new JButton("방만들기");
 		createRoomButton.setBounds(299, 53, 104, 34);
 		createRoomButton.setFont(new Font("HY엽서M", Font.PLAIN, 15));
 		createRoomButton.addMouseListener(new MouseAdapter() {
 			
 			@Override
+			// 방만들기 버튼 입력시 실행
 			public void mouseClicked(MouseEvent e) {
+				// 방 제목 입력받는 창이 뜸
 				roomName = JOptionPane.showInputDialog(chattingRoomListPanel, "방제목을 입력하세요.");
-				if(Objects.isNull(roomName)) {	//취소버튼을 눌렀을 경우 null 이 들어가면서 이벤트 종료
+				// 취소버튼 입력이 이벤트 종료
+				if(Objects.isNull(roomName)) {	
 					return;
 				}
-				if(roomName.isBlank()) {	//방제목에 공백이 들어갔을 시 에러메시지를 띄어주면서 리턴걸어서 빠져나감.
+				// 공백인 경우 에러 메세지 표시 후 이벤트 종료
+				if(roomName.isBlank()) {	
 					JOptionPane.showMessageDialog(chattingRoomListPanel, "방제목을 입력하세요.", "방만들기 실패", JOptionPane.ERROR_MESSAGE);
 					return;
 				}
-				// roomListModel 안에 roomName이 들어있는데 동일한 방제목이 있을 시 에러메시지 띄어주고 리턴(마우스 이벤트 메소드 빠져나가는 리턴)
+				// 중복인 경우 메세지 표시 후 이벤트 종료
 				for(int i = 0; i < roomListModel.size(); i++) {
 					if(roomListModel.get(i).equals(roomName)) {
 						JOptionPane.showMessageDialog(chattingRoomListPanel, "이미 존재하는 방제목입니다.", "방만들기 실패", JOptionPane.ERROR_MESSAGE);
 						return;
 					}
 				}
-				// 정상적인 방제목입력 할 시 정상 처리
+				// 정상적인 방제목 입력시 실행
+				// 크리에이티드룸 타입의 생성 후 데이터를 requestBodyDto 할당 (소켓의 커넥션)
 				RequestBodyDto<String> requestBodyDto = new RequestBodyDto<String>("createRoom", roomName);
+				// 데이터를 클라이언트로 전송
 				ClientSender.getInstance().send(requestBodyDto);
+				// 채팅창으로 이동
 				mainCardLayout.show(mainCardPanel, "chattingRoomPanel");
+				// 조인 타입의 생성 후 데이터를 requestBodyDto 할당 (소켓의 커넥션)
 				requestBodyDto = new RequestBodyDto<String>("join", roomName);
+				// 데이터를 클라이언트로 전송
 				ClientSender.getInstance().send(requestBodyDto);
+				// 라벨에 "방제목 : roomName" 표시 
 				roomLabel.setText("방제목:   " + roomName);
+				// 들어갔을시 채팅입력창 활성화
                 messageTextField.setEnabled(true);
 				
 			}
@@ -292,7 +317,7 @@ public class SimpleGUIClient extends JFrame {
 		mainCardPanel.add(chattingRoomPanel, "chattingRoomPanel");
 		chattingRoomPanel.setLayout(null);
 		
-		//방제목라벨
+		// 방제목라벨
 		roomLabel = new JLabel("방제목: ");
 		roomLabel.setBounds(45, 10, 244, 19);
 		chattingRoomPanel.add(roomLabel);
@@ -302,7 +327,7 @@ public class SimpleGUIClient extends JFrame {
 		roomLabel.setForeground(Color.BLUE);
 		chattingRoomPanel.add(roomLabel);
 		
-		//채팅창
+		// 채팅창
 		JScrollPane chattingTextAreaScrollPane = new JScrollPane();
 		chattingTextAreaScrollPane.setBounds(12, 39, 277, 406);
 		chattingTextArea = new JTextArea();
@@ -313,7 +338,7 @@ public class SimpleGUIClient extends JFrame {
 		chattingTextArea.setDisabledTextColor(Color.BLACK);
 		chattingRoomPanel.add(chattingTextAreaScrollPane);
 
-		//채팅방안 유저리스트
+		// 채팅방안 유저리스트
 		userListScrollPane = new JScrollPane();
 		userListScrollPane.setBounds(294, 39, 109, 406);
 		chattingRoomPanel.add(userListScrollPane);
@@ -322,27 +347,29 @@ public class SimpleGUIClient extends JFrame {
 		userList = new JList(userListModel);
 		userList.setFont(new Font("맑은 고딕", Font.BOLD, 11));
 		
-		//userList 두번클릭시 인덱스번호 들고와서 내자신을 from유저에 , index번호에있는 user를 to유저에 집어넣음
 		userList.addMouseListener(new MouseAdapter() {
 		@Override
 	    public void mouseClicked(MouseEvent e) {
+			// 유저리스트 더블클릭시 실행
 	        if (e.getClickCount() == 2) {
+	        	// 선택한 유저리스트 인덱스를 selectedIndex 할당
 	            selectedIndex = userList.getSelectedIndex();
+	            // 만약 0이상인 경우 실행
 	            if (selectedIndex >= 0) {
-	            	
+	            	// selectedIndex에서 selectedIndex값을 가져와 fromUsername 할당
 	                fromUsername = userListModel.getElementAt(selectedIndex);
-	                
+	                // 만약 "방장"이라는 포함 되어 있으면 해당 부분 제거
 	                if(fromUsername.contains("( 방장 )")) {
 	                	fromUsername = fromUsername.replace("( 방장 )", "");
 	                }
-	                
+	                // 라벨에 표시
 	                TargetLabel.setText(fromUsername);
-
+	                // 귓속말 객체를 생성
 	                whisperMessage = SendMessage.builder()
 	                        .fromUsername(username)
 	                        .toUsername(fromUsername)
 	                        .build();
-	                
+	                // 채팅입력창을 활성화시키고 포커스로 설정
 	                messageTextField.setEnabled(true);
 	                messageTextField.requestFocus();
 	            }
@@ -352,14 +379,14 @@ public class SimpleGUIClient extends JFrame {
 		
 		userListScrollPane.setViewportView(userList);
 		
-		//전체,귓말 라벨
+		// 전체,귓말 라벨
 		TargetLabel = new JLabel("전체");
 		TargetLabel.setFont(new Font("HY엽서M", Font.PLAIN, 12));
 		TargetLabel.setHorizontalAlignment(JLabel.CENTER);
 		TargetLabel.setBounds(12, 486, 55, 29);
 		chattingRoomPanel.add(TargetLabel);
 		
-		//채팅입력창
+		// 채팅입력창
 		messageTextField = new JTextField();
 		messageTextField.setFont(new Font("맑은 고딕", Font.PLAIN, 11));
 		
@@ -396,19 +423,20 @@ public class SimpleGUIClient extends JFrame {
 		chattingRoomPanel.add(messageTextField);
 		messageTextField.setColumns(10);
 		
-		//채팅입력창 전송버튼
+		// 채팅입력창 전송버튼
 		sendButton = new JButton("전송");
 		sendButton.setFont(new Font("HY엽서M", Font.PLAIN, 12));
 		sendButton.addMouseListener(new MouseAdapter() {	
 
-			//전송버튼을 눌렀을때 targetLabel이 user면 귓속말 전체면 공개채팅임 귓말후 모두 초기화시켜줌
 		    @Override
+		    // 전송버튼 클릭시 실행
 		    public void mouseClicked(MouseEvent e) {
 		        String messageBody = messageTextField.getText();
-
-
+		        // 라벨에 전체가 아닌 경우 실행
 		        if (!TargetLabel.getText().equals("전체")) {
+		        	// 귓속말의 데이터를 업데이트
 		        	whisperMessage.setMessageBody(messageBody);
+		        	
 		            RequestBodyDto<SendMessage> requestBodyDto =
 		                    new RequestBodyDto<>("sendWhisperMessage", whisperMessage);
 		            ClientSender.getInstance().send(requestBodyDto);
--- server_socket/src/server_socket/SimpleGUIserver.java
+++ server_socket/src/server_socket/SimpleGUIserver.java
@@ -11,19 +11,26 @@ import server_socket.entity.Room;
 
 public class SimpleGUIserver {
 	
+	// 연결된 소켓의 리스트를 저장하는 공간
 	public static List<ConnectedSocket> connectedSocketList = new ArrayList<>();
+	// 방 생성시 방의 리스트를 저장하는 공간
 	public static List<Room> roomList = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		try {
+			// 8000 포트에서 서버 생성
 			ServerSocket serverSocket = new ServerSocket(8000);
 			System.out.println("[ 서버실행 ]");
 			
 			while(true) {
+				// 클라이언트 연결을 기다리는 중
 				Socket socket = serverSocket.accept();
 				System.out.println("접속");
+				// 소켓에 객체 생성 (쓰레드)
 				ConnectedSocket connectedSocket = new ConnectedSocket(socket);
+				// 쓰레드 실행
 				connectedSocket.start();
+				// 소켓리스트에 추가 
 				connectedSocketList.add(connectedSocket);
 			}
 		}catch (IOException e) {
